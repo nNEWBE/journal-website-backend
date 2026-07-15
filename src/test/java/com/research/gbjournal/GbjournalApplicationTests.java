96ac39fabc4edddb546ad645b990acba3db906f3
nNEWBE <shuvochandra999@gmail.com>
2026-07-15 11:39:15 +0600

    project initialized

---
+++ src/test/java/com/research/gbjournal/GbjournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.research.gbjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbjournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
